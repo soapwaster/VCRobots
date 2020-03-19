3c0e2be57d86a69d38335970d1393a570b31a5fe
soapwaster <colitta.valerio@gmail.com>
2020-03-19 19:02:42 +0100

    Position2D: add comments

--- robot-game/src/main/java/com/soapwaster/vcr/robot_game/Position2D.java
+++ robot-game/src/main/java/com/soapwaster/vcr/robot_game/Position2D.java
@@ -1,5 +1,9 @@
 package com.soapwaster.vcr.robot_game;
 
+/**
+ * Represents a point in the 2D space of the game
+ *
+ */
 public class Position2D {
 	
 	private int x;
@@ -9,8 +13,7 @@ public class Position2D {
 		this.x = x;
 		this.y = y;
 		
-		this.x = ((x % Game.MAX_X) + Game.MAX_X) % Game.MAX_X;
-		this.y = ((y % Game.MAX_Y) + Game.MAX_Y) % Game.MAX_Y;
+		normalize();
 		
 	}
 	
@@ -36,6 +39,9 @@ public class Position2D {
 		return y;
 	}
 	
+	/**
+	 * Normalizes the point, so that if it exceeds game limits, it just restart from 0. Makes it circular
+	 */
 	public void normalize() {
 		x = ((x % Game.MAX_X) + Game.MAX_X) % Game.MAX_X;
 		y = ((y % Game.MAX_Y) + Game.MAX_Y) % Game.MAX_Y;
