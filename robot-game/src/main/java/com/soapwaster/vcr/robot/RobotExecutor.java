945a8f262de3679946beb92306756431840dd0ec
soapwaster <colitta.valerio@gmail.com>
2020-03-19 19:06:08 +0100

    RobotExecutor: make it use EventTypeEnum

--- robot-game/src/main/java/com/soapwaster/vcr/robot/RobotExecutor.java
+++ robot-game/src/main/java/com/soapwaster/vcr/robot/RobotExecutor.java
@@ -2,6 +2,7 @@ package com.soapwaster.vcr.robot;
 
 import com.soapwaster.vcr.event_handling.DeadEvent;
 import com.soapwaster.vcr.event_handling.Event;
+import com.soapwaster.vcr.event_handling.EventTypeEnum;
 import com.soapwaster.vcr.event_handling.MoveToEvent;
 import com.soapwaster.vcr.event_handling.ShootAtEvent;
 import com.soapwaster.vcr.event_handling.StatEnchantEvent;
@@ -25,9 +26,10 @@ public class RobotExecutor {
 			return;
 		}
 		
-		String eventName = event.getClass().getSimpleName(); 
+		EventTypeEnum eventName = event.getType(); 
+		
 		switch (eventName) {
-		case "MoveToEvent":{
+		case MoveTo:{
 			
 			MoveToEvent moveEvent = (MoveToEvent) event;
 			Position2D movePos = moveEvent.getPosition();
@@ -37,36 +39,35 @@ public class RobotExecutor {
 			System.out.println(robot.toString() + " moved to " + moveEvent.getPosition());
 			break;
 		}
-		case "ShootAtEvent":{
+		case ShootAt:{
 			
 			ShootAtEvent shootEvent = (ShootAtEvent) event;
-			System.out.println(robot + " " + robot.getPosition()+" : " + event.getSource() + " shot at " + shootEvent.getPosition());
+			
 			robot.notifySupportShoot(shootEvent.getPosition());
 			if(robot.receiveDamageIn(shootEvent.getPosition())) {
 				robot.decreaseHealth(shootEvent.getDamage());
 				robot.notifySupportHit();
-				if(robot.getHealth() <= 0.0) {
-					System.out.println(robot + " got hit");
+				if(robot.getHealth() <= 0) {
 					Game.getInstance().getMainEventDispatcher().addEvent(new DeadEvent(robot, robot));
 				}
-				break;
 			}
-			System.out.println(robot + " did not get hit");
+			System.out.println(robot + " " + robot.getPosition()+" : " + event.getSource() + " shot at " + shootEvent.getPosition());
 			break;
 		}
-		case "DeadEvent":{
+		case Dead:{
 			
 			System.out.println(robot + " just died");
 			Game.getInstance().removeRobot(robot);
-			Game.getInstance().getMainEventDispatcher().removeListener(robot);
+			Game.getInstance().getMainEventDispatcher().unregisterListener(robot);
 			robot.dead = true;
 			break;
 		}
-		case "StatEnchantEvent": {
+		case StatEnchant: {
 			StatEnchantEvent statEvent = (StatEnchantEvent) event;
 			robot.setStat(statEvent.getStatEnchanter());
-			System.out.println(robot + " : receiving powerup, " + robot.getStat());
 			robot.notifyStatChange();
+			
+			System.out.println(robot + " : receiving powerup, " + robot.getStat());
 			break;
 		}
 		default:
