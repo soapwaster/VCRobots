290037fb5bf061498ecfd53c350b0bb8deafdfcb
soapwaster <colitta.valerio@gmail.com>
2020-03-20 11:18:15 +0100

    StatsIncreaser: renaming and stop condition

--- robot-game/src/main/java/com/soapwaster/vcr/stats/StatsIncreaser.java
+++ robot-game/src/main/java/com/soapwaster/vcr/stats/StatsIncreaser.java
@@ -6,33 +6,43 @@ import com.soapwaster.vcr.event_handling.StatEnchantEvent;
 import com.soapwaster.vcr.robot.Robot;
 import com.soapwaster.vcr.robot_game.Game;
 
-public class StatIncreaser implements Runnable{
+public class StatsIncreaser implements Runnable{
 	
 	private List<Robot> robotPlayers;
+	private boolean running = true;
+	private long powerupInterval = 100;
 	
-	public StatIncreaser(List<Robot> robotPlayers) {
+	public StatsIncreaser(List<Robot> robotPlayers) {
 		super();
 		this.robotPlayers = robotPlayers;
+	}
+	
+	public StatsIncreaser(List<Robot> robotPlayers, long powerupInterval) {
+		this(robotPlayers);
+		this.powerupInterval  = powerupInterval;
 	}
 
 	@Override
 	public void run() {
 		
-		while(true) {
+		while(running) {
 			int rand = (int) (Math.random() * robotPlayers.size());
 			Robot chosenRobot = robotPlayers.get(rand);
-		
-			Stats statEnchanter = new StatsFactory(chosenRobot).getRandomStat();
+		    Stats statEnchanter = new StatsFactory(chosenRobot).getRandomStat();
 			
 			Game.getInstance().getMainEventDispatcher().addEvent(new StatEnchantEvent(chosenRobot, chosenRobot, statEnchanter));
 			
+			//Wait a little before gifting the next powerup
 			try {
-				Thread.sleep(200);
+				Thread.sleep(powerupInterval);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public void stop() {
+		running = false;
 	}
 
 }
