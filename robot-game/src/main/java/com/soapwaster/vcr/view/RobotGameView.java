f9d946c047d61ffda92ca81e9ef85aacf217c61d
soapwaster <colitta.valerio@gmail.com>
2020-03-20 11:17:25 +0100

    RobotGameView: refactor to handle an arbitrary number of robot players

--- robot-game/src/main/java/com/soapwaster/vcr/view/RobotGameView.java
+++ robot-game/src/main/java/com/soapwaster/vcr/view/RobotGameView.java
@@ -3,6 +3,8 @@ package com.soapwaster.vcr.view;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -14,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 
 import javax.swing.SwingConstants;
@@ -22,39 +23,58 @@ import javax.swing.SwingConstants;
 import com.soapwaster.vcr.robot.Robot;
 import com.soapwaster.vcr.robot_game.Game;
 import com.soapwaster.vcr.robot_game.Position2D;
-import javax.swing.JSeparator;
 import javax.swing.border.LineBorder;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
-public class RobotView2 implements PropertyChangeListener{
+import java.awt.Font;
+import javax.swing.JTextPane;
+
+/**
+ * The main View that shows the Robot battle.
+ * Contains :
+ * <ul>
+ * <li> Stats for each robot (hp, range, damage)
+ * <li> Robot battlefield
+ * <li> Log section that describes every event happening in the battlefield
+ * <li> A winned section that declares the winner at the end of the battle
+ * </ul>
+ *
+ */
+public class RobotGameView implements PropertyChangeListener{
 
 	private JFrame frame;
 	private List<Robot> robots;
 	private Map<Robot, RobotViewStats> robotViewStats;
-	private RobotViewPanel panel_2;
+	private RobotViewPanel robotBattlePanel;
+	private JLabel winnerLabel;
+	private JTextPane logPane;
 
 	/**
-	 * Create the application.
+	 * Creates a RobotGame view.
 	 */
-	public RobotView2(List<Robot> robots) {
+	public RobotGameView(List<Robot> robots) {
 		this.robots = robots;
 		robotViewStats = new HashMap<Robot, RobotViewStats>();
+		Color robotColors[] = new Color[] {Color.ORANGE, Color.BLUE, Color.RED, Color.DARK_GRAY};
 		for (Robot robot : robots) {
 			RobotViewStats rvs = new RobotViewStats();
 			JLabel hpLabel = new JLabel(String.valueOf(robot.getHealth()));
 			JLabel rangeLabel = new JLabel(String.valueOf(robot.getStat().getRange()));
 			JLabel damageLabel = new JLabel(String.valueOf(robot.getStat().getDamage()));
+			
 			rvs.setHpLabel(hpLabel);
 			rvs.setDamageLabel(damageLabel);
 			rvs.setRangeLabel(rangeLabel);
+			
 			robotViewStats.put(robot, rvs);
+			robotViewStats.get(robot).setColor(robotColors[robotViewStats.size()-1]);
 			robot.addPropertyChangeListener(this);
 		}
-		robotViewStats.get(robots.get(0)).setColor(Color.BLACK);
-		robotViewStats.get(robots.get(1)).setColor(Color.BLUE);
-		robotViewStats.get(robots.get(2)).setColor(Color.GREEN);
-		robotViewStats.get(robots.get(3)).setColor(Color.RED);
 		
-		panel_2 = new RobotViewPanel(robotViewStats);
+		robotBattlePanel = new RobotViewPanel(robotViewStats);
 		initialize();
 	}
 
@@ -66,165 +86,124 @@ public class RobotView2 implements PropertyChangeListener{
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.EAST);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new LineBorder(Color.GRAY));
-		panel_1.add(panel_3);
-		panel_3.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblRn = new JLabel(robots.get(0).toString());
-		lblRn.setForeground(robotViewStats.get(robots.get(0)).getColor());
-		lblRn.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblRn, BorderLayout.NORTH);
-		
-		JPanel panel_7 = new JPanel();
-		panel_3.add(panel_7, BorderLayout.CENTER);
-		panel_7.setLayout(new GridLayout(3, 2, 0, 0));
-		
-		JLabel lblHp = new JLabel("Hp: ");
-		panel_7.add(lblHp);
-		
-		panel_7.add(robotViewStats.get(robots.get(0)).getHpLabel());
-		
-		JLabel lblNewLabel_2 = new JLabel("Range:");
-		panel_7.add(lblNewLabel_2);
-		
-		panel_7.add(robotViewStats.get(robots.get(0)).getRangeLabel());
-		
-		JLabel lblDamage = new JLabel("Damage:");
-		panel_7.add(lblDamage);
-		
-		panel_7.add(robotViewStats.get(robots.get(0)).getDamageLabel());
-		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBorder(new LineBorder(Color.GRAY));
-		panel_1.add(panel_5);
-		panel_5.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_4 = new JPanel();
-		panel_5.add(panel_4, BorderLayout.CENTER);
-		panel_4.setLayout(new GridLayout(3, 2, 0, 0));
-		
-		JLabel lblNewLabel_1 = new JLabel("Hp: ");
-		panel_4.add(lblNewLabel_1);
-		
-		panel_4.add(robotViewStats.get(robots.get(1)).getHpLabel());
-		
-		JLabel lblNewLabel_5 = new JLabel("Range:");
-		panel_4.add(lblNewLabel_5);
-		
-		panel_4.add(robotViewStats.get(robots.get(1)).getRangeLabel());
+		JPanel overallStatsPanel = new JPanel();
+		frame.getContentPane().add(overallStatsPanel, BorderLayout.EAST);
+		overallStatsPanel.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		
-		JLabel lblNewLabel_6 = new JLabel("Damage:");
-		panel_4.add(lblNewLabel_6);
+		for (Robot robot : robots) {
+			JPanel statsPanel = new JPanel();
+			statsPanel.setBorder(new LineBorder(Color.GRAY));
+			overallStatsPanel.add(statsPanel);
+			statsPanel.setLayout(new BorderLayout(0, 0));
+			
+			//Robot name label setup
+			JLabel robotNameLabel = new JLabel(robot.toString());
+			robotNameLabel.setForeground(robotViewStats.get(robot).getColor());
+			robotNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			statsPanel.add(robotNameLabel, BorderLayout.NORTH);
+			
+			JPanel gridPanel = new JPanel();
+			statsPanel.add(gridPanel, BorderLayout.CENTER);
+			gridPanel.setLayout(new GridLayout(3, 2, 0, 0));
+			 
+			//HP info 
+			gridPanel.add(new JLabel("Hp: "));
+			gridPanel.add(robotViewStats.get(robot).getHpLabel());
+			
+			//Range info
+			gridPanel.add(new JLabel("Range:"));
+			gridPanel.add(robotViewStats.get(robot).getRangeLabel());
+			
+			//Damage info
+			gridPanel.add(new JLabel("Damage:"));
+			gridPanel.add(robotViewStats.get(robot).getDamageLabel());
+		}
 		
-		panel_4.add(robotViewStats.get(robots.get(1)).getDamageLabel());
 		
-		JLabel lblNewLabel = new JLabel(robots.get(1).toString());
-		lblNewLabel.setForeground(robotViewStats.get(robots.get(1)).getColor());
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_5.add(lblNewLabel, BorderLayout.NORTH);
+		frame.setSize(new Dimension(900, 550));
+		frame.getContentPane().add(robotBattlePanel, BorderLayout.CENTER);
 		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBorder(new LineBorder(Color.GRAY));
-		panel_1.add(panel_6);
-		panel_6.setLayout(new BorderLayout(0, 0));
+		winnerLabel = new JLabel();
+		winnerLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		winnerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(winnerLabel, BorderLayout.NORTH);
 		
-		JLabel lblNewLabel_3 = new JLabel(robots.get(2).toString());
-		lblNewLabel_3.setForeground(robotViewStats.get(robots.get(2)).getColor());
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_6.add(lblNewLabel_3, BorderLayout.NORTH);
-		
-		JPanel panel_8 = new JPanel();
-		panel_6.add(panel_8);
-		panel_8.setLayout(new GridLayout(3, 2, 0, 0));
-		
-		
-		JLabel lblHp_1 = new JLabel("Hp: ");
-		panel_8.add(lblHp_1);
-		
-		
-		panel_8.add(robotViewStats.get(robots.get(2)).getHpLabel());
-		
-		JLabel lblNewLabel_8 = new JLabel("Range:");
-		panel_8.add(lblNewLabel_8);
-		
-		panel_8.add(robotViewStats.get(robots.get(2)).getRangeLabel());
-		
-		JLabel lblNewLabel_9 = new JLabel("Damage:");
-		panel_8.add(lblNewLabel_9);
-		
-		panel_8.add(robotViewStats.get(robots.get(2)).getDamageLabel());
-		
-		JPanel panel_9 = new JPanel();
-		panel_9.setBorder(new LineBorder(Color.GRAY));
-		panel_1.add(panel_9);
-		panel_9.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_4 = new JLabel(robots.get(3).toString());
-		lblNewLabel_4.setForeground(robotViewStats.get(robots.get(3)).getColor());
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_9.add(lblNewLabel_4, BorderLayout.NORTH);
-		
-		JPanel panel_10 = new JPanel();
-		panel_9.add(panel_10);
-		panel_10.setLayout(new GridLayout(3, 2, 0, 0));
-		
-		JLabel lblHp_2 = new JLabel("Hp: ");
-		panel_10.add(lblHp_2);
-		
-		panel_10.add(robotViewStats.get(robots.get(3)).getHpLabel());
-		
-		JLabel lblNewLabel_10 = new JLabel("Range:");
-		panel_10.add(lblNewLabel_10);
-		
-		panel_10.add(robotViewStats.get(robots.get(3)).getRangeLabel());
-		
-		JLabel lblNewLabel_11 = new JLabel("Damage:");
-		panel_10.add(lblNewLabel_11);
-		
-		panel_10.add(robotViewStats.get(robots.get(3)).getDamageLabel());
-		
-	
-		frame.setSize(new Dimension(700, 400));
-		frame.getContentPane().add(panel_2, BorderLayout.CENTER);
+		logPane = new JTextPane();
+		JScrollPane scrollLogPane = new JScrollPane(logPane);
+		scrollLogPane.setPreferredSize(new Dimension(frame.getSize().width, 200));
+		frame.getContentPane().add(scrollLogPane, BorderLayout.SOUTH);
 		
 	}
 
+	/**
+	 * Makes the battle ground visible
+	 */
+	public void showBattleGround() {
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Updates the game battle view by drawing robots in their positions together with their last hit mark
+	 */
+	private void redrawView() {
+		robotBattlePanel.revalidate();
+		robotBattlePanel.repaint();
+	}
+
+	/**
+	 * Handles a subject property change notification
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		String pName = evt.getPropertyName();
-		switch (pName) {
-		case "hit":{
+		PropertyChangeType pcType = PropertyChangeType.valueOf(pName); 
+		switch (pcType) {
+		case Hit:{
 			for (Robot robot : robots) {
-				robotViewStats.get(robot).getHpLabel().setText(String.valueOf(robot.getHealth()));
+				String health = String.valueOf(robot.getHealth());
+				robotViewStats.get(robot).getHpLabel().setText(health);
 			}
 			break;	
 		}
-		case "stat":{
+		case Stats:{
 			for (Robot robot : robots) {
-				robotViewStats.get(robot).getRangeLabel().setText(String.valueOf(robot.getStat().getRange()));
-				robotViewStats.get(robot).getDamageLabel().setText(String.valueOf(robot.getStat().getDamage()));
-
+				String range = String.valueOf(robot.getStat().getRange());
+				String damage = String.valueOf(robot.getStat().getDamage());
+				robotViewStats.get(robot).getRangeLabel().setText(range);
+				robotViewStats.get(robot).getDamageLabel().setText(damage);
 			}
+			break;
+		}
+		case Move:{
+			redrawView();
 			break;	
 		}
-		case "move":{
-			panel_2.revalidate();
-			panel_2.repaint();
-			
-			break;	
-		}
-		case "shoot":{
+		case Shoot:{
 			Position2D shootPos = (Position2D) evt.getNewValue();
 			Robot source = (Robot) evt.getSource();
-			robotViewStats.get(source).setLastHitmark(new Position2D(shootPos.getX(), shootPos.getY()));
-			panel_2.revalidate();
-			panel_2.repaint();
+			
+			robotViewStats.get(source).setLastHitmark(shootPos);
+			
+			redrawView();
+			break;	
+		}
+		case Win:{
+			Robot source = (Robot) evt.getSource();
+			
+			winnerLabel.setText(source + " won the battle with " + (int) source.getHealth() + " HP left!");
+			
+			redrawView();
+			break;	
+		}
+		case Log:{
+			Robot source = (Robot) evt.getSource();
+			String logDescription = (String) evt.getNewValue();
+			
+			appendToPane(logPane, source.toString(), robotViewStats.get(source).getColor());
+		    appendToPane(logPane, ": " + logDescription + "\n\n", Color.BLACK);
+			
+			redrawView();
 			break;	
 		}
 
@@ -234,18 +213,33 @@ public class RobotView2 implements PropertyChangeListener{
 		
 	}
 	
-	public void show() {
-		frame.setVisible(true);
-	}
-	
+	/**
+	 * Appends colored message a JTextPane
+	 * @param textPane 
+	 * @param msg the message to append
+	 * @param color the color of the message
+	 */
+	private void appendToPane(JTextPane textPane, String msg, Color color)
+    {
+        StyleContext sc = StyleContext.getDefaultStyleContext();
+        AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, color);
+
+        aset = sc.addAttribute(aset, StyleConstants.FontFamily, "Lucida Console");
+        aset = sc.addAttribute(aset, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
+
+        int len = textPane.getDocument().getLength();
+        textPane.setCaretPosition(len);
+        textPane.setCharacterAttributes(aset, false);
+        textPane.replaceSelection(msg);
+    }
 
 }
 
+/**
+ * The panel that shows in real time the battle
+ */
 class RobotViewPanel extends JPanel {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	private Map<Robot, RobotViewStats> robotViewStats;
@@ -261,23 +255,30 @@ class RobotViewPanel extends JPanel {
         	if(robot.isDead()) {
         		continue;
         	}
-        	double normalizedX = (double) robot.getPosition().getX()/Game.MAX_X * this.getSize().width;
-        	double normalizedY = (double) robot.getPosition().getY()/Game.MAX_Y * this.getSize().height;
-       
         	
+        	Position2D robotPosition = getScaledPosition(robot.getPosition());
         	g.setColor(robotViewStats.get(robot).getColor());
-        	g.fillRect((int) normalizedX, (int) normalizedY, 10, 10);
+        	g.fillRect(robotPosition.getX(), robotPosition.getY(), 10, 10);
         	
-        	
-        	normalizedX = (double) robotViewStats.get(robot).getLastHitmark().getX()/Game.MAX_X * this.getSize().width;
-        	normalizedY = (double) robotViewStats.get(robot).getLastHitmark().getY()/Game.MAX_Y * this.getSize().height;
-        	
-        	int lineSize = 5;
-        	
-        	g.drawLine((int)normalizedX -lineSize, (int)normalizedY -lineSize, (int)normalizedX  + lineSize, (int)normalizedY +lineSize);
-        	g.drawLine((int)normalizedX +lineSize, (int)normalizedY -lineSize, (int)normalizedX  - lineSize, (int)normalizedY +lineSize);
+        	int lineSize = 4;
+        	Position2D hitmarkPosition = getScaledPosition(robotViewStats.get(robot).getLastHitmark());
+        	g.setColor(robotViewStats.get(robot).getColor());
+        	g.drawLine((int)hitmarkPosition.getX() -lineSize, (int)hitmarkPosition.getY() -lineSize, (int)hitmarkPosition.getX()  + lineSize, (int)hitmarkPosition.getY() +lineSize);
+        	g.drawLine((int)hitmarkPosition.getX() +lineSize, (int)hitmarkPosition.getY() -lineSize, (int)hitmarkPosition.getX()  - lineSize, (int)hitmarkPosition.getY() +lineSize);
 
         }
     }
+
+	/**
+	 * Scales the position to accomodate panel size
+	 * @param position
+	 * @return scaled position
+	 */
+	private Position2D getScaledPosition(Position2D position) {
+		double scaledX = (double) position.getX()/Game.WIDTH * this.getSize().width;
+    	double scaledY = (double) position.getY()/Game.HEIGHT * this.getSize().height;
+    	
+    	return new Position2D((int)scaledX, (int)scaledY, false);
+	}
 	
 }
